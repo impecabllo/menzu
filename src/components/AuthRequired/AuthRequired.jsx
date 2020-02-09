c13f0af82adae18f22cd8c055eaffba7c99d6486
impecabllo <impecabllo@gmail.com>
2020-02-09 21:00:14 +0300

    AuthRequired fix

--- src/components/AuthRequired/AuthRequired.jsx
+++ src/components/AuthRequired/AuthRequired.jsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Redirect } from 'react-router'
 
 const AuthRequiredComponent = (props) => {
-  return localStorage.getItem('email') ? <>{ props.children }</> : <Redirect to={ '/auth' } />
+  return localStorage.getItem('email') ? <>{ props.children }</> : <Redirect to={ '/sign-in' } />
 }
 
 export const AuthRequired = (Component) => (props) =>  <AuthRequiredComponent><Component {...props} /></AuthRequiredComponent>
